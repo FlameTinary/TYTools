543212d8dbe5b31e47700c7f00550755a531f0cb
Sheldon <tinarychina@gmail.com>
2022-06-05 11:25:14 +0800

    UIColor扩展方法支持hex字符串转颜色

--- TYTools/Classes/TYCategories/UIColor+TYColorExtension.h
+++ TYTools/Classes/TYCategories/UIColor+TYColorExtension.h
@@ -9,15 +9,24 @@
 #import <UIKit/UIKit.h>
 
 @interface UIColor (TYColorExtension)
-//随机颜色
+/// 随机颜色
 + (UIColor *)randomColor;
-/**
- * 十六进制的颜色值转换为颜色UIColor（带透明度）
- */
-+(UIColor*) colorWithHex:(NSInteger)hexValue alpha:(CGFloat)alphaValue;
 
-/**
- * 十六进制的颜色值转换为颜色UIColor
- */
-+(UIColor*) colorWithHex:(NSInteger)hexValue;
+
+/// 十六进制的颜色值转换为颜色UIColor（带透明度）
+/// @param hexValue 十六进制value
+/// @param alphaValue 透明度 0~1
++ (UIColor*)colorWithHex:(UInt32)hexValue alpha:(CGFloat)alphaValue;
+
+
+/// 十六进制的颜色值转换为颜色UIColor
+/// @param hexValue 十六进制
++ (UIColor*)colorWithHex:(UInt32)hexValue;
+
+
+/// 十六进制字符串转color
+/// @param hexString 十六进制字符串
++ (UIColor *)colorWithHexString:(NSString *)hexString;
+
++ (UIColor *)colorWithHexString:(NSString *)hexString alpha:(CGFloat)alpha;
 @end
--- TYTools/Classes/TYCategories/UIColor+TYColorExtension.m
+++ TYTools/Classes/TYCategories/UIColor+TYColorExtension.m
@@ -15,15 +15,26 @@
     return color;
 }
 
-+ (UIColor*) colorWithHex:(NSInteger)hexValue alpha:(CGFloat)alphaValue
++ (UIColor*) colorWithHex:(UInt32)hexValue alpha:(CGFloat)alphaValue
 {
     return [UIColor colorWithRed:((float)((hexValue & 0xFF0000) >> 16))/255.0
                            green:((float)((hexValue & 0xFF00) >> 8))/255.0
                             blue:((float)(hexValue & 0xFF))/255.0 alpha:alphaValue];
 }
 
-+ (UIColor*) colorWithHex:(NSInteger)hexValue
++ (UIColor*) colorWithHex:(UInt32)hexValue
 {
     return [UIColor colorWithHex:hexValue alpha:1.0];
+}
+
++ (UIColor *)colorWithHexString:(NSString *)hexString {
+    return [UIColor colorWithHexString:hexString alpha:1];
+}
+
++ (UIColor *)colorWithHexString:(NSString *)hexString alpha:(CGFloat)alpha {
+    NSScanner *scanner = [NSScanner scannerWithString:hexString];
+    unsigned hexNum;
+    if (![scanner scanHexInt:&hexNum]) return nil;
+    return [UIColor colorWithHex:hexNum alpha:alpha];
 }
 @end
